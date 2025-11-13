1ffadc3ddae64e841a2a6fdd4d5a50ec7c1d83b0
gche3 <gche3@illinois.edu>
2025-11-13 17:36:09 -0600

    fixed a few warnings

--- app/src/main/java/com/example/onpaw/FindPetCareActivity.kt
+++ app/src/main/java/com/example/onpaw/FindPetCareActivity.kt
@@ -2,7 +2,6 @@ package com.example.onpaw
 
 import android.content.Intent
 import android.os.Bundle
-import android.provider.Settings
 import android.text.InputType
 import android.view.Menu
 import android.view.MenuItem

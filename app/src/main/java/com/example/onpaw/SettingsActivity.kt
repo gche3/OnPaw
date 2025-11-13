d8a5bb2ccdf09b9252bebb71f6ea9b826a04d511
gche3 <gche3@illinois.edu>
2025-11-13 15:03:53 -0600

    fixed warnings

--- app/src/main/java/com/example/onpaw/SettingsActivity.kt
+++ app/src/main/java/com/example/onpaw/SettingsActivity.kt
@@ -4,7 +4,6 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
-import android.widget.SearchView
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 

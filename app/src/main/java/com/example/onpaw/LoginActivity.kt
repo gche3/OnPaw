7fa63a58720c1b9457667634df076bf6b6eeb494
gche3 <gche3@illinois.edu>
2025-11-13 18:47:56 -0600

    merged charles' files and added skeleton for login and signup functionality

---
+++ app/src/main/java/com/example/onpaw/LoginActivity.kt
@@ -0,0 +1,13 @@
+package com.example.onpaw
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login)
+
+    }
+}
